1cee1c3040eca81ed24d01220c4e1ef571d97f38
Joe Contreras <joe.contreras888@gmail.com>
2022-03-17 13:55:53 -0600

    Adds user to db.json

--- src/components/register-component/Register.jsx
+++ src/components/register-component/Register.jsx
@@ -1,55 +1,99 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import './Register.css';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import "./Register.css";
+import axios from "axios";
 
 export default function Register() {
-  function submit() {
-    console.log('submited');
-  }
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      // toast.error("Passwords do not match");
+      console.log("Passwords do not match");
+    } else {
+      try {
+        const res = await axios.post("http://localhost:4000/users", {
+          name,
+          email,
+          password,
+        });
+        console.log(res);
+      } catch (e) {
+        console.log(e);
+      }
+      // dispatch(register(userData));
+    }
+  };
   return (
-    <Grid container direction='row' justifyContent='center' alignItems='center'>
+    <Grid container direction="row" justifyContent="center" alignItems="center">
       <div>
         <Box
-          component='form'
+          component="form"
           sx={{
-            '& .MuiTextField-root': { m: 1, width: '25ch' },
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
           }}
           noValidate
-          autoComplete='off'
+          autoComplete="off"
         >
           <h1>Register Component</h1>
           <TextField
-            className='input-field'
-            id='name'
-            label='Name'
-            variant='standard'
-          />{' '}
+            className="input-field"
+            id="name"
+            label="Name"
+            variant="standard"
+            name="name"
+            value={name}
+            onChange={onChange}
+          />{" "}
           <br />
           <TextField
-            className='input-field'
-            id='standard-basic'
-            label='Email'
-            variant='standard'
+            className="input-field"
+            id="standard-basic"
+            label="Email"
+            variant="standard"
+            name="email"
+            value={email}
+            onChange={onChange}
           />
           <br />
           <TextField
-            className='input-field'
-            id='standard-basic'
-            label='Password'
-            variant='standard'
+            className="input-field"
+            id="standard-basic"
+            label="Password"
+            variant="standard"
+            name="password"
+            value={password}
+            onChange={onChange}
           />
           <br />
           <TextField
-            className='input-field'
-            id='standard-basic'
-            label='Confirm Password'
-            variant='standard'
+            className="input-field"
+            id="standard-basic"
+            label="Confirm Password"
+            variant="standard"
+            name="password2"
+            value={password2}
+            onChange={onChange}
           />
         </Box>
-        <Button onClick={submit} variant='contained'>
+        <Button onClick={onSubmit} variant="contained">
           Send
         </Button>
       </div>
